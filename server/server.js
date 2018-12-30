d7bb15f5c47ed527c7af82c139bd46aa640b9259
Andreas Schultz <andreasschultz@hotmail.com>
2018-12-30 20:43:39 +0100

    Adjusted static serve

--- server/server.js
+++ server/server.js
@@ -51,7 +51,10 @@ app.use('/assessment', assessment);
 
 // ====SERVE BUILD FOLDER====
 app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('*', (req, res) => res.sendFile(path.resolve('../client/build', 'index.html'));
+// app.get('*', (req, res) => res.sendFile(path.resolve('../client/build', 'index.html')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});	
 
 
 app.listen(port);
